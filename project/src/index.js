e03f7c7b5a73ebd9f47f30fb0cb2be52daef5fbb
Exjoy23 <exjoy23@gmail.com>
2021-06-02 20:50:52 +0300

    добавит первые React-компоненты

--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const OFFERS_COUNT = 5;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App offersCount={OFFERS_COUNT} />
   </React.StrictMode>,
-  document.getElementById('root'));
+  document.getElementById('root'),
+);
